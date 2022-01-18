dc6909410e7197cda47d78bc3b314d15eaf9921e
Keijiro Takahashi <keijiro@gmail.com>
2022-01-18 17:22:13 +0900

    Several test cases

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,11 +5,98 @@ using StringBuilder = System.Text.StringBuilder;
 
 public sealed class Test : MonoBehaviour
 {
-    #if !UNITY_EDITOR && PLATFORM_IOS
+    #if !UNITY_EDITOR && (UNITY_IOS || UNITY_WEBGL)
     const string _dll = "__Internal";
     #else
     const string _dll = "testbed";
     #endif
+
+    //
+    // Bool args/return
+    //
+
+    [DllImport(_dll)]
+    [return: MarshalAs(UnmanagedType.U1)]
+    static extern bool get_xor([MarshalAs(UnmanagedType.U1)] bool a,
+                               [MarshalAs(UnmanagedType.U1)] bool b);
+
+    void RunBoolTest()
+    {
+        Debug.Log($"true ^ true = {get_xor(true, true)}");
+        Debug.Log($"true ^ false = {get_xor(true, false)}");
+    }
+
+    //
+    // Struct layout
+    //
+
+    struct Data1
+    {
+        public byte   d8;
+        public ushort d16;
+        public uint   d32;
+        public ulong  d64;
+    }
+
+    struct Data2
+    {
+        [MarshalAs(UnmanagedType.U1)] public bool d1;
+        [MarshalAs(UnmanagedType.U1)] public bool d2;
+        public float  d32;
+        public double d64;
+    }
+
+    [DllImport(_dll)]
+    static extern void modify_struct_data1(in Data1 src, out Data1 dst);
+
+    [DllImport(_dll)]
+    static extern void modify_inplace_struct_data1(ref Data1 data);
+
+    [DllImport(_dll)]
+    static extern void modify_struct_data2(in Data2 src, out Data2 dst);
+
+    [DllImport(_dll)]
+    static extern void modify_inplace_struct_data2(ref Data2 data);
+
+    static void RunStructTest()
+    {
+        var data1 = new Data1
+        {
+            d8  = 0x12,
+            d16 = 0x3456,
+            d32 = 0x789abcdeu,
+            d64 = 0x123456789abcdef0ul
+        };
+
+        var data2 = new Data2
+        {
+            d1  = false,
+            d2  = true,
+            d32 = Mathf.PI,
+            d64 = System.Math.PI
+        };
+
+        var temp1 = new Data1();
+        var temp2 = new Data2();
+
+        modify_struct_data1(in data1, out temp1);
+        modify_struct_data2(in data2, out temp2);
+
+        modify_inplace_struct_data1(ref data1);
+        modify_inplace_struct_data2(ref data2);
+
+        Debug.Log("Data1 (must be 13, 3457, 789ABCDF, 123456789ABCDEF1)");
+        Debug.Log($"{temp1.d8:X} {temp1.d16:X} {temp1.d32:X} {temp1.d64:X}");
+        Debug.Log($"{data1.d8:X} {data1.d16:X} {data1.d32:X} {data1.d64:X}");
+
+        Debug.Log("Data2 (must be true, false, pi+1, pi+1)");
+        Debug.Log($"{temp2.d1} {temp2.d2} {temp2.d32} {temp2.d64}");
+        Debug.Log($"{data2.d1} {data2.d2} {data2.d32} {data2.d64}");
+    }
+
+    //
+    // String operations
+    //
 
     [DllImport(_dll)]
     static extern uint count_substring(string s, string sub);
@@ -23,26 +110,33 @@ public sealed class Test : MonoBehaviour
     [DllImport(_dll)]
     static extern IntPtr format_float(float value);
 
-    [DllImport(_dll)]
-    [return: MarshalAs(UnmanagedType.U1)]
-    static extern bool get_xor([MarshalAs(UnmanagedType.U1)] bool a,
-                               [MarshalAs(UnmanagedType.U1)] bool b);
-    
-
-    void Start()
+    static void RunStringTest()
     {
-        var str = "hi hi high ho hige";
-        var sub = "hi";
+        var str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        var sub = "do";
 
-        Debug.Log(count_substring(str, sub));
-        Debug.Log(count_substring2(new StringBuilder(str), sub));
+        var c1 = count_substring(str, sub);
+        var c2 = count_substring2(new StringBuilder(str), sub);
+
+        Debug.Log($"Substring search (should be 6): {c1}");
+        Debug.Log($"With StringBuilder: {c2}");
 
         var sb = new StringBuilder(256);
         set_string(sb, sb.Capacity + 1);
-        Debug.Log(sb);
+        Debug.Log($"Modified string: {sb}");
 
-        Debug.Log(Marshal.PtrToStringAnsi(format_float(Mathf.PI)));
+        var fmt = Marshal.PtrToStringAnsi(format_float(Mathf.PI));
+        Debug.Log($"Returned string: {fmt}");
+    }
 
-        Debug.Log($"{get_xor(true, true)}, {get_xor(false, true)}");
+    //
+    // MonoBehaviour entry point
+    //
+
+    void Start()
+    {
+        RunBoolTest();
+        RunStructTest();
+        RunStringTest();
     }
 }
--- Plugin/src/lib.rs
+++ Plugin/src/lib.rs
@@ -2,6 +2,71 @@ use std::ffi::CStr;
 use std::ffi::CString;
 use std::os::raw::c_char;
 
+//
+// Bool args/return
+//
+
+#[no_mangle]
+pub unsafe extern fn get_xor(a: bool, b: bool) -> bool {
+    a ^ b
+}
+
+//
+// Struct layout
+//
+
+#[repr(C)]
+pub struct Data1 {
+    d8: u8,
+    d16: u16,
+    d32: u32,
+    d64: u64
+}
+
+#[repr(C)]
+pub struct Data2 {
+    d1: bool,
+    d2: bool,
+    d32: f32,
+    d64: f64
+}
+
+#[no_mangle]
+pub unsafe extern fn modify_struct_data1(src: *const Data1, dst: *mut Data1) {
+    (*dst).d8  = (*src).d8  + 1;
+    (*dst).d16 = (*src).d16 + 1;
+    (*dst).d32 = (*src).d32 + 1;
+    (*dst).d64 = (*src).d64 + 1;
+}
+
+#[no_mangle]
+pub unsafe extern fn modify_inplace_struct_data1(data: *mut Data1) {
+    (*data).d8  += 1;
+    (*data).d16 += 1;
+    (*data).d32 += 1;
+    (*data).d64 += 1;
+}
+
+#[no_mangle]
+pub unsafe extern fn modify_struct_data2(src: *const Data2, dst: *mut Data2) {
+    (*dst).d1  = (*src).d1 ^ true;
+    (*dst).d2  = (*src).d2 ^ true;
+    (*dst).d32 = (*src).d32 + 1.0;
+    (*dst).d64 = (*src).d64 + 1.0;
+}
+
+#[no_mangle]
+pub unsafe extern fn modify_inplace_struct_data2(data: *mut Data2) {
+    (*data).d1 ^= true;
+    (*data).d2 ^= true;
+    (*data).d32 += 1.0;
+    (*data).d64 += 1.0;
+}
+
+//
+// String operations
+//
+
 #[no_mangle]
 pub unsafe extern fn count_substring(str_ptr: *const c_char, sub_ptr: *const c_char) -> u32 {
     let string = CStr::from_ptr(str_ptr).to_str().unwrap();
@@ -26,9 +91,4 @@ pub unsafe extern fn format_float(value: f32) -> *const c_char {
     FORMAT_FLOAT_BUFFER.resize(bytes.len(), 0);
     FORMAT_FLOAT_BUFFER.copy_from_slice(bytes);
     FORMAT_FLOAT_BUFFER.as_ptr() as *const c_char
-}
-
-#[no_mangle]
-pub unsafe extern fn get_xor(a: bool, b: bool) -> bool {
-    a ^ b
 }
